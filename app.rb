196e877336153ca58de6e98e59f7c412ab2d412d
Komica20160704 <bluecrazybear@gmail.com>
2016-10-08 23:59:28 +0800

    Rng background

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require 'sinatra/base'
 
 module HomuApi
   class App < Sinatra::Base
-    get "/" do
+    get '/' do
       count = request.env['WsClientCount']
-      erb :index, :locals => { :css_list => ['index.css'], :ws_client_count => count }
+      bg = Dir.glob('./public/bgs/*.png').map { |i| File.basename i }.sample
+      erb :index, locals: { css_list: ['index.css'], ws_client_count: count, bg: bg }
     end
 
-    run! if app_file == $0
+    run! if app_file == $PROGRAM_NAME
   end
 end
