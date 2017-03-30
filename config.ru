db9aeeaab9c51fb69f50d1c873ab2fb927dae971
Komica20160704 <bluecrazybear@gmail.com>
2017-03-30 23:01:31 +0800

    AAHAHHAAAHA

--- config.ru
+++ config.ru
@@ -5,6 +5,8 @@ require './middlewares/homu_notifier'
 
 Faye::WebSocket.load_adapter('thin') if $0.match /thin$/
 
+$stdout.sync = true
+
 use HomuApi::HomuChecker
 use HomuApi::HomuNotifier
 run HomuApi::App.new
--- helper/homu_getter.rb
+++ helper/homu_getter.rb
@@ -7,11 +7,25 @@ module HomuApi
     @url = 'http://homu-homuapi.rhcloud.com/'
 
     def self.get_page page = '0'
-      open(@url + '/page/' + page).read
+      begin
+        return open(@url + '/page/' + page).read
+      rescue OpenURI::HTTPError => e
+        puts 'OpenURI::HTTPError'
+        puts e.message
+        puts e.backtrace
+        return '{}'
+      end
     end
 
     def self.get_res res
-      open(@url + '/res/' + res).read
+      begin
+        return open(@url + '/res/' + res).read
+      rescue OpenURI::HTTPError => e
+        puts 'OpenURI::HTTPError'
+        puts e.message
+        puts e.backtrace
+        return '{}'
+      end
     end
   end
 end
