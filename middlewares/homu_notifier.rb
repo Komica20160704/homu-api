e91b8a5841aabd959e9f61679eb2113656f05ba3
Komica20160704 <bluecrazybear@gmail.com>
2018-01-15 23:20:11 +0800

    sayonara 2017

--- middlewares/homu_notifier.rb
+++ middlewares/homu_notifier.rb
@@ -16,44 +16,6 @@ module HomuApi
     def notify(data)
       @clients.each { |client| client.send(data.to_json) }
       log_data data
-      if data['Type'] == 'Notify' && !data['Blocks'].nil?
-        noty_all(data)
-      end
-    end
-
-    def noty_all(data)
-      attachments = data['Blocks'].map do |block|
-        attachemnt = {
-          fallback: message(block),
-
-          author_name: block['Name'],
-          title: block['Title'],
-          text: "#{block['Content']}",
-          ts: Time.parse("#{block['Date']} #{block['Time']} +0800").to_i,
-        }
-        if block['HeadNo'] != block['No']
-          head = data['Heads'].find { |head| head['No'] == block['HeadNo'] }
-          attachemnt[:pretext] = "#{block['Id']}回應了<http://rem.komica2.net/00/pixmicat.php?res=#{block['HeadNo']}|討論串>"
-          attachemnt[:author_name] = "No.#{head['No']} ID:#{head['Id']}: #{head['Content'].gsub("\n", ' ')[0..8]}⋯⋯\n"
-          attachemnt[:color] = '#f0e0d6'
-        else
-          attachemnt[:pretext] = "#{block['Id']}發了一篇<http://rem.komica2.net/00/pixmicat.php?res=#{block['HeadNo']}|新文章>"
-          attachemnt[:color] = '#ffffee'
-        end
-        if block['Picture']
-          attachemnt[:image_url] = "http://p2.komica.ml/00/src/#{block['Picture']}"
-          attachemnt[:thumb_url] = "http://p2.komica.ml/00/thumb/#{block['Picture'].split('.').first}s.jpg"
-        end
-        attachemnt
-      end
-      NotyAllWorker.perform_async attachments
-    end
-
-    def message(block)
-      result = "#{block['Title']} #{block['Name']} #{block['Date']} #{block['Time']} ID:#{block['Id']} No.#{block['No']}\n"
-      result += "http://p2.komica.ml/00/thumb/#{block['Picture'].split('.').first}s.jpg\n" if block['Picture']
-      result += block['Content']
-      result
     end
 
     def log_data(data)
--- workers/workers.rb
+++ workers/workers.rb
@@ -1,2 +1,1 @@
 require_relative './say_hi_worker'
-require_relative './noty_all_worker'
