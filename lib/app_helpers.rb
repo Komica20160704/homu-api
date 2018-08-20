d3ba07d109f73df6acff2baf0986412da086773f
Komica20160704 <bluecrazybear@gmail.com>
2018-08-20 20:03:28 +0800

    Hatobatsugu

--- lib/app_helpers.rb
+++ lib/app_helpers.rb
@@ -5,9 +5,14 @@ module AppHelpers
     settings.env == 'production'
   end
 
+  def homu_url
+    return 'https://www.homu-api.com' if production?
+    'https://homu-api.dev'
+  end
+
   def homu_api_url
-    return 'https://homu.homu-api.com/' if production?
-    'https://api-homu.dev/'
+    return 'https://homu.homu-api.com' if production?
+    'https://api-homu.dev'
   end
 
   def vue_js_url
